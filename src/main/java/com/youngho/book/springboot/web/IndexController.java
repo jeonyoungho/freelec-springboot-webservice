9d2533d93ad08fafdb6f061fe41489237e330083
jeonyoungho <yhjun1000@gmail.com>
2024-10-06 21:27:42 +0900

    chapter5 구글 소셜 로그인 기능 추가

--- src/main/java/com/youngho/book/springboot/web/IndexController.java
+++ src/main/java/com/youngho/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.youngho.book.springboot.web;
 
+import com.youngho.book.springboot.config.auth.dto.SessionUser;
 import com.youngho.book.springboot.service.PostsService;
 import com.youngho.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession session;
 
     @GetMapping("/")
     public String index(Model model) {
 
         model.addAttribute("posts", postsService.findAllDesc());
 
+        SessionUser user = (SessionUser) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
